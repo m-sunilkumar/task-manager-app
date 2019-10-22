b080b0106d237d43ad3197fc5c8245e1fd58edb4
m-sunilkumar <sunilbhargav060@gmail.com>
2019-10-22 18:05:13 +0530

    init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 const express = require("express");
-const userRoutes=require('./routes/user')
-const tasksRoutes=require('./routes/tasks')
+const userRoutes = require("./routes/user");
+const tasksRoutes = require("./routes/tasks");
 
 require("./db/mongoose");
 
 const app = express();
 app.use(express.json());
 app.use(userRoutes);
-app.use(tasksRoutes)
+app.use(tasksRoutes);
 
 const port = process.env.PORT;
-
-
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
